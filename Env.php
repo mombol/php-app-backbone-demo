6643ee9f4d816bcf66d86cc9508cd5abc6247dd0
mombol <mombol@163.com>
2016-04-20 17:37:41 +0800

    add enviroment define format judge

--- Env.php
+++ Env.php
@@ -6,6 +6,12 @@ class Env
 {
     public static function get()
     {
+        if (!defined('ENVIROMENT')) {
+            throw new \InvalidArgumentException("ENVIROMENT is undefined!");
+        }
+        if (!preg_match('/[a-zA-Z_][a-zA-Z0-9_]*/', ENVIROMENT)) {
+            throw new \InvalidArgumentException("ENVIROMENT format error!");
+        }
         return ENVIROMENT;
     }
 }
