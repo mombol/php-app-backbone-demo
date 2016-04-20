f0eeaa54979f3bfc6663aac263d193c439c9bcb3
mombol <mombol@163.com>
2016-04-20 17:39:07 +0800

    modify enviroment define format judge

--- Env.php
+++ Env.php
@@ -9,9 +9,6 @@ class Env
         if (!defined('ENVIROMENT')) {
             throw new \InvalidArgumentException("ENVIROMENT is undefined!");
         }
-        if (!preg_match('/[a-zA-Z_][a-zA-Z0-9_]*/', ENVIROMENT)) {
-            throw new \InvalidArgumentException("ENVIROMENT format error!");
-        }
         return ENVIROMENT;
     }
 }
